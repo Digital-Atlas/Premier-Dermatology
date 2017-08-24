fd1d5b6a08d05d411088c2b5535ed13b6b3e5945
LD <ldao@digital-atlas.com>
2017-08-24 10:52:35 -0700

    Cosmetic landing page iteration

--- page-templates/cosmetic-services.php
+++ page-templates/cosmetic-services.php
@@ -65,7 +65,7 @@ get_template_part( 'template-parts/section', 'hero' );
 					}
 				
 					
-					section_collage();
+					//section_collage();
 
 					function section_collage() {
 
@@ -85,21 +85,74 @@ get_template_part( 'template-parts/section', 'hero' );
 						// check if the repeater field has rows of data
 						if( have_rows('list_of_benefits') ):					
 
+							printf('<div id="benefits-list"><div class="row benefits small-12 medium-12">');
+
 						 	// loop through the rows of data
 						    while ( have_rows('list_of_benefits') ) : the_row();
-								printf('<div id="benefits-list"><div class="row benefits small-10 medium-12">');
-						        printf('<div class="column small-2 medium-2">%s</div>', get_sub_field('icon') );
-						        printf('<div class="column small-10 medium-10"><strong>%s</strong><br />%s</div>', get_sub_field('benefit_title'), get_sub_field('benefit_text'));						        
-						        printf('</div></div>');
+
+								$image = sprintf('<img src="%s" alt="Image" />', get_sub_field('image'));
+								$title = get_sub_field('benefit_title');
+								$copy = get_sub_field('benefit_text');
+
+						        printf('<div class="column small-12 medium-12 large-4">%s<div class="inner"><h5>%s</h5><p>%s<p></div></div>', $image, $title, $copy);
+						        //printf('<div class="column small-10 medium-10"><strong>%s</strong><br />%s</div>', get_sub_field('benefit_title'), get_sub_field('benefit_text'));						        
+						       
 						    endwhile;
 
-						else :
+						     printf('</div></div>');
 
-						    // no rows found
 						endif;
-						
-
 					}
+
+					// Infographics ACF Repeater
+					section_infographic_stats();
+					function section_infographic_stats() {
+
+						// check if the repeater field has rows of data
+						if( have_rows('infographic_stats') ):					
+
+							printf('<div class="infographic-stats row small-12 medium-12">');
+
+						 	// loop through the rows of data
+						    while ( have_rows('infographic_stats') ) : the_row();
+
+								$icon = sprintf('<img src="%s" alt="Icon" />', get_sub_field('icon'));
+								$stat = get_sub_field('stat');
+
+						        printf('<div class="column small-12 medium-6 large-6"><div class="inner"><div class="small-4 column icon">%s</div><div class="small-8 column data">%s</div></div></div>', $icon, $stat);
+						       
+						    endwhile;
+
+						     printf('</div>');
+
+						endif;
+					}
+
+
+					// Testimonial and Video 
+					section_cosmetic_testimonial();
+					function section_cosmetic_testimonial() {
+
+							printf('<div class="patient-review small-12 medium-12">');
+
+								$testimonial = get_field('cosmetic_testimonial');
+								$video_id = get_field('cosmetic_featured_video');
+
+								if( !empty( $video_id ) ) {
+					 				$video  = _s_get_foobox_video( $video_id );	
+								}
+
+							printf( '<div class="small-12 large-4 column testimonial"><span>%s</span></div><div class="small-12 large-8 column video">%s</div>', $testimonial, $video );								
+
+
+						  
+						       
+						     printf('</div>');
+					}
+
+
+
+
 
 					
 					// Featured Content
